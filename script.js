760d02f2f6cd07ea48ca21f5adae3bdf8f298d20
marcinmagala <marcinmagala@gmail.com>
2022-08-28 00:17:54 +0200

    app is completed without: [2]
    everything works fine without:
    - local storage
    - RWD

--- script.js
+++ script.js
@@ -10,6 +10,8 @@ const sortByAllBtn = document.querySelector('.sort_by_all');
 const sortByActiveBtn = document.querySelector('.sort_by_active');
 const sortByCompletedBtn = document.querySelector('.sort_by_completed');
 const clearBtn = document.querySelector('.clear');
+const itemsLeft = document.querySelector('.items_left');
+// const lefts = document.querySelector('.lefts');
 
 class ToDo {
   status = 'activ';
@@ -21,6 +23,10 @@ class ToDo {
 
 class App {
   #tasks = [];
+  #sortByActive = [];
+  #sortByCompleted = [];
+  #sortActiveStatus = false;
+  #sortCompletedStatus = false;
 
   constructor() {
     addBtn.addEventListener('click', this._newTask.bind(this));
@@ -56,6 +62,7 @@ class App {
 
       // Update task on list
       this._updateList(this.#tasks);
+      this._updateTasksLeft(this.#tasks);
     }
   }
 
@@ -79,41 +86,125 @@ class App {
     });
   }
 
+  _renderLeftItems(task) {
+    let leftTask = task.filter(item => item.status === 'activ');
+    let leftItems = `<p class="lefts">${leftTask.length} ${
+      leftTask.length === 1 || leftTask.length === 0 ? 'item' : 'items'
+    } left</p>`;
+
+    itemsLeft.insertAdjacentHTML('afterbegin', leftItems);
+  }
+
   _taskStatusChanged(e) {
     e.preventDefault();
     // To wygląda tak źle dlatego że musi działać obojętne czy user kliknie w ikonkę czy w tło ikonki - rozwiązaniem tego byłoby gdyby ikonka miała tło więc wtedy dałoby się zdefiniować ją jako tło do diva i nie byłoby różnicy czy user kliknie w tło czy w ikonkę ponieważ byłby to jeden i ten sam element. Do momentu przerobienia ikonki musi tak to zostać.
-    if (e.target.closest('.circle') !== null) {
-      e.target.classList.add('hidden');
-      e.target.nextSibling.nextSibling.classList.remove('hidden');
-      e.target.closest('.app_form').dataset.status = 'completed';
-      console.log(e.target);
-      // Zmiana stanu statusu
-      this.#tasks.find(
-        task => task.id === e.target.closest('.app_form').dataset.id
-      ).status = 'completed';
-      console.log(this.#tasks);
-      //
-    } else if (e.target.nextSibling.nextSibling === null) {
-      e.target.closest('.circle-completed').classList.add('hidden');
-      e.target
-        .closest('.circle-completed')
-        .nextSibling.parentNode.firstElementChild.classList.remove('hidden');
-      e.target.closest('.app_form').dataset.status = 'activ';
-      // Zmiana stanu statusu
-      this.#tasks.find(
-        task => task.id === e.target.closest('.app_form').dataset.id
-      ).status = 'activ';
-      //
-    } else if (e.target.nextSibling.nextSibling !== null) {
-      e.target.classList.add('hidden');
-      e.target.nextSibling.parentNode.firstElementChild.classList.remove(
-        'hidden'
-      );
-      e.target.closest('.app_form').dataset.status = 'activ';
-      // Zmiana stanu statusu
-      this.#tasks.find(
-        task => task.id === e.target.closest('.app_form').dataset.id
-      ).status = 'activ';
+
+    if (true) {
+      if (e.target.closest('.circle') !== null) {
+        console.log(this.#tasks);
+        e.target.classList.add('hidden');
+        e.target.nextSibling.nextSibling.classList.remove('hidden');
+        e.target.closest('.app_form').dataset.status = 'completed';
+
+        // Zmiana stanu statusu w obiekcie #tasks
+        this.#tasks.find(
+          task => task.id === e.target.closest('.app_form').dataset.id
+        ).status = 'completed';
+        console.log(this.#tasks);
+        // console.log(this.#tasks);
+
+        // Blokada zmiany widoku listy jeśli jest posortowana
+        // if (
+        //   this.#sortCompletedStatus === false &&
+        //   this.#sortActiveStatus === false
+        // ) {
+        //   this._updateList(this.#tasks);
+        // } else if (
+        //   this.#sortCompletedStatus === true &&
+        //   this.#sortActiveStatus === false
+        // ) {
+        //   this._updateList(this.#sortByCompleted);
+        // } else if (
+        //   this.#sortCompletedStatus === false &&
+        //   this.#sortActiveStatus === true
+        // ) {
+        //   this._updateList(this.#sortByActive);
+
+        // }
+        this._updateList(this.#tasks);
+        this._updateTasksLeft(this.#tasks);
+        //
+      } else if (e.target.nextSibling.nextSibling === null) {
+        console.log(this.#tasks);
+        if (
+          e.target.closest('.circle-completed').classList.add('hidden') !== null
+        ) {
+          e.target.closest('.circle-completed').classList.add('hidden');
+          e.target
+            .closest('.circle-completed')
+            .nextSibling.parentNode.firstElementChild.classList.remove(
+              'hidden'
+            );
+          e.target.closest('.app_form').dataset.status = 'activ';
+          // Zmiana stanu statusu w obiekcie #tasks
+          this.#tasks.find(
+            task => task.id === e.target.closest('.app_form').dataset.id
+          ).status = 'activ';
+
+          // Blokada zmiany widoku listy jeśli jest posortowana
+          // if (
+          //   this.#sortCompletedStatus === false &&
+          //   this.#sortActiveStatus === false
+          // ) {
+          //   this._updateList(this.#tasks);
+          // } else if (
+          //   this.#sortCompletedStatus === true &&
+          //   this.#sortActiveStatus === false
+          // ) {
+          //   this._updateList(this.#sortByCompleted);
+          // } else if (
+          //   this.#sortCompletedStatus === false &&
+          //   this.#sortActiveStatus === true
+          // ) {
+          //   this._updateList(this.#sortByActive);
+          // }
+          this._updateList(this.#tasks);
+          this._updateTasksLeft(this.#tasks);
+        }
+        //
+      } else if (e.target.nextSibling.nextSibling !== null) {
+        console.log(this.#tasks);
+        e.target.classList.add('hidden');
+        e.target.nextSibling.parentNode.firstElementChild.classList.remove(
+          'hidden'
+        );
+        if (e.target.closest('.app_form') !== null) {
+          e.target.closest('.app_form').dataset.status = 'activ';
+          // Zmiana stanu statusu w obiekcie #tasks
+          this.#tasks.find(
+            task => task.id === e.target.closest('.app_form').dataset.id
+          ).status = 'activ';
+          // Blokada zmiany widoku listy jeśli jest posortowana
+          // if (
+          //   this.#sortCompletedStatus === false &&
+          //   this.#sortActiveStatus === false
+          // ) {
+          //   this._updateList(this.#tasks);
+          // } else if (
+          //   this.#sortCompletedStatus === true &&
+          //   this.#sortActiveStatus === false
+          // ) {
+          //   this._updateList(this.#sortByCompleted);
+          // } else if (
+          //   this.#sortCompletedStatus === false &&
+          //   this.#sortActiveStatus === true
+          // ) {
+          //   this._updateList(this.#sortByActive);
+          // }
+          this._updateList(this.#tasks);
+          this._updateTasksLeft(this.#tasks);
+        }
+      }
     }
   }
 
@@ -124,46 +215,70 @@ class App {
     sortByActiveBtn.classList.remove('sort_by_this_option');
     sortByAllBtn.classList.add('sort_by_this_option');
     sortByCompletedBtn.classList.remove('sort_by_this_option');
+
+    this.#sortActiveStatus = false;
+    this.#sortCompletedStatus = false;
   }
 
   //   Sorting by activ
   _sortByActive(e) {
     e.preventDefault();
+    console.log(this.#tasks);
+
+    this.#sortByActive = this.#tasks.filter(task => task.status === 'activ');
+    this._updateList(this.#sortByActive);
+
     sortByActiveBtn.classList.add('sort_by_this_option');
     sortByAllBtn.classList.remove('sort_by_this_option');
     sortByCompletedBtn.classList.remove('sort_by_this_option');
-    let sortByActive = this.#tasks.filter(task => task.status === 'activ');
-    this._updateList(sortByActive);
+
+    this.#sortActiveStatus = true;
+    this.#sortCompletedStatus = false;
   }
 
   //   Sorting by completed
   _sortByCompleted(e) {
     e.preventDefault();
+    console.log(this.#tasks);
+    this.#sortByCompleted = this.#tasks.filter(
+      task => task.status === 'completed'
+    );
+    this._updateList(this.#sortByCompleted);
+
     sortByActiveBtn.classList.remove('sort_by_this_option');
     sortByAllBtn.classList.remove('sort_by_this_option');
     sortByCompletedBtn.classList.add('sort_by_this_option');
 
-    let sortbyCompleted = this.#tasks.filter(
-      task => task.status === 'completed'
-    );
-    this._updateList(sortbyCompleted);
+    this.#sortActiveStatus = false;
+    this.#sortCompletedStatus = true;
   }
 
   _clearCompleted(e) {
     e.preventDefault();
+    let taskcomplited = this.#tasks.filter(item => item.status === 'activ');
 
-    this.#tasks.forEach((task, i) => {
-      if (task.status === 'completed') {
-        this.#tasks.splice(i, 1);
-      }
-    });
+    this.#tasks = taskcomplited;
+    console.log(this.#tasks);
+
     this._updateList(this.#tasks);
+    this._updateTasksLeft(this.#tasks);
   }
 
   _updateList(tasks) {
     const appListItem = document.querySelectorAll('.app_list--item');
     appListItem.forEach(item => item.remove());
+    // Powrót do sortowania po wszystkich tasks
+    sortByActiveBtn.classList.remove('sort_by_this_option');
+    sortByAllBtn.classList.add('sort_by_this_option');
+    sortByCompletedBtn.classList.remove('sort_by_this_option');
+
     this._renderTask(tasks);
+  }
+
+  _updateTasksLeft(tasks) {
+    const lefts = document.querySelector('.lefts');
+    lefts.remove();
+    this._renderLeftItems(tasks);
   }
 }
 
